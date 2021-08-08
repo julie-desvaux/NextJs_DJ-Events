42ce0df74e8ead480fd737c68459fea0d345f9b5
julie-desvaux <desvaux.julie@gmail.com>
2021-08-08 19:54:15 +0200

    Module Aliases With jsconfig

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,9 @@
 import { FaExclamationTriangle } from "react-icons/fa";
 import Link from "next/link";
 
-import Layout from "../components/Layout";
+import Layout from "@/components/Layout";
 
-import styles from "../styles/404.module.css";
+import styles from "@/styles/404.module.css";
 
 export default function NotFoundPage() {
 	return (
